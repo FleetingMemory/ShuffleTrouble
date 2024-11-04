a3ab23091780fb64f028fecbb86940ccc193583c
ronit-086 <ronitkhanujaa@gmail.com>
2024-11-04 22:11:33 +0530

    Added New Game Scenes
    Created Player Screen And Scores Screen
    Initial Implementation  Of Leaderboard

---
+++ src/main/java/com/example/shuffletrouble/PlayerScene.java
@@ -0,0 +1,16 @@
+package com.example.shuffletrouble;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class PlayerScene extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
