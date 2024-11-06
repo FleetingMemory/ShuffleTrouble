e0a431d838c4531182322e591c976ef38e0c5ca7
ronit-086 <ronitkhanujaa@gmail.com>
2024-11-07 00:53:54 +0530

    Ui design for Player Screen
    Styling the  Enter Name , Start and Back Button

--- src/main/java/com/example/shuffletrouble/PlayerController.java
+++ src/main/java/com/example/shuffletrouble/PlayerController.java
@@ -15,20 +15,21 @@ import static javafx.application.ConditionalFeature.FXML;
 
 public class PlayerController {
 
-    public void manageLeaderboard(ArrayList<Pair<String , Integer>> leaderboard){
+    public void manageLeaderboard(ArrayList<Pair<String, Integer>> leaderboard) {
         System.out.print(leaderboard);
     }
+
     private Stage stage;
     private Scene scene;
     private Parent root;
 
     //Function to direct player to game screen
-    public void switchToGameScreen(ActionEvent event){
-        try{
+    public void switchToGameScreen(ActionEvent event) {
+        try {
 
             FXMLLoader loader = new FXMLLoader(getClass().getResource("GameScene.fxml"));
             root = loader.load();
-            stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+            stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
             scene = new Scene(root);
             stage.setScene(scene);
             stage.show();
@@ -36,6 +37,22 @@ public class PlayerController {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-
     }
-}
+
+        public void switchToMain (ActionEvent event){
+            try {
+
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("Main.fxml"));
+                root = loader.load();
+                stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+                scene = new Scene(root);
+                stage.setScene(scene);
+                stage.show();
+
+
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
